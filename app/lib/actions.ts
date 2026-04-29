3d6b8842220c2d1c63d103dfe2fbbd9c776c7d6e
vanja-veapi <vanja.veapi@gmail.com>
2026-04-29 16:16:10 +0200

    Chapter 14: Adding Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
@@ -112,3 +114,22 @@ export type State = {
 	};
 	message?: string | null;
 };
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData,
+) {
+	try {
+		await signIn('credentials', formData);
+	} catch (error) {
+		if (error instanceof AuthError) {
+			switch (error.type) {
+				case 'CredentialsSignin':
+					return 'Invalid credentials.';
+				default:
+					return 'Something went wrong.';
+			}
+		}
+		throw error;
+	}
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,20 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
+import { Suspense } from 'react';
+
+export default function LoginPage() {
+	return (
+		<main className="flex justify-center items-center md:h-screen">
+			<div className="relative flex flex-col space-y-2.5 mx-auto md:-mt-32 p-4 w-full max-w-[400px]">
+				<div className="flex items-end bg-blue-500 p-3 rounded-lg w-full h-20 md:h-36">
+					<div className="w-32 md:w-36 text-white">
+						<AcmeLogo />
+					</div>
+				</div>
+				<Suspense>
+					<LoginForm />
+				</Suspense>
+			</div>
+		</main>
+	);
+}
